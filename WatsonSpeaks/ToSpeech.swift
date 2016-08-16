9f26ca206d1061effeeaa68f587ee49a50fa0e52
Sarah Chen <sarahchen22@gmail.com>
2016-08-16 17:26:45 -0500

    Simplified functions to overall synthesizeVoice func, and added loadVoices function for another call to watson service. Added notification functionality to notify viewController when data is successfully received back from the server

--- WatsonSpeaks/ToSpeech.swift
+++ WatsonSpeaks/ToSpeech.swift
@@ -14,18 +14,22 @@ class ToSpeech {
     
     private var tts: TextToSpeech!      /* tts object to use TextToSpeech. */
     var player: AVAudioPlayer?          /* Object used to play audio in app. */
+    var voiceNames: [String]
     
     /* Service's credentials to authenticate. */
     init(username: String, password: String) {
         tts = TextToSpeech(username: username, password: password)
+        self.voiceNames = []
     }
     
     /* Synthesize text to audio. Kate selected for which voice to be used for synthesis.
      * Returns the text in audio format specified and uses callback to output the audio.
     */
-    func kateSpeaking(text: String) {
+    
+    //
+    func synthesizeVoice(text: String, voice: SynthesisVoice) {
         tts.synthesize(text,
-                       voice: SynthesisVoice.GB_Kate,
+                       voice: voice,
                        audioFormat: AudioFormat.WAV,
                        failure: { error in
                         print("error was generated \(error)")
@@ -41,61 +45,21 @@ class ToSpeech {
         }
     }
     
-    /* Synthesize text to audio with Allison's voice. */
-    func allisonSpeaking(text: String) {
-        tts.synthesize(text,
-                       voice: SynthesisVoice.US_Allison,
-                       audioFormat: AudioFormat.WAV,
-                       failure: { error in
-                        print("error was generated \(error)")
-        }) { data in
-            
-            do {
-                self.player = try AVAudioPlayer(data: data)
-                self.player!.play()
-            } catch {
-                print("Couldn't create player.")
+    func loadVoices() {
+        var voices:[Voice] = []
+        tts.getVoices({ error in
+            print("error was generated \(error)")
+            }) { data in
+                voices = data
+                for voice in voices {
+                    self.voiceNames.append(voice.name)
+                }
+                self.receivedDataFromServer()
             }
-            
-        }
     }
     
-    /* Synthesize text to audio with Michael's voice. */
-    func michaelSpeaking(text: String) {
-        tts.synthesize(text,
-                       voice: SynthesisVoice.US_Michael,
-                       audioFormat: AudioFormat.WAV,
-                       failure: { error in
-                        print("error was generated \(error)")
-        }) { data in
-            
-            do {
-                self.player = try AVAudioPlayer(data: data)
-                self.player!.play()
-            } catch {
-                print("Couldn't create player.")
-            }
-            
-        }
-    }
-    
-    /* Synthesize text to audio with Lisa's voice. */
-    func lisaSpeaking(text: String) {
-        tts.synthesize(text,
-                       voice: SynthesisVoice.US_Lisa,
-                       audioFormat: AudioFormat.WAV,
-                       failure: { error in
-                        print("error was generated \(error)")
-        }) { data in
-            
-            do {
-                self.player = try AVAudioPlayer(data: data)
-                self.player!.play()
-            } catch {
-                print("Couldn't create player.")
-            }
-            
-        }
+    func receivedDataFromServer() {
+        NSNotificationCenter.defaultCenter().postNotificationName("ReceivedData", object: nil)
     }
     
 }
