7258851aea35b0890bfcf65d6ee31105b5e47ce6
Sarah Chen <sarahchen22@gmail.com>
2016-08-24 15:53:19 -0500

    adding Credentials file

---
+++ Credentials.swift
@@ -0,0 +1,16 @@
+//
+//  Credentials.swift
+//  WatsonSpeaks
+//
+//  Created by Sarah Chen on 8/10/16.
+//  Copyright © 2016 IBM-MIL. All rights reserved.
+//
+
+import Foundation
+
+class Credentials {
+    static let sharedInstance = Credentials()
+    
+    var username = "YOUR USERNAME HERE"
+    var password = "YOUR PASSWORD HERE"
+}
