d90bd6f67b281fa7370a44557838653a9ca804be
Horea Porutiu <horeaporutiu@gmail.com>
2017-06-11 20:10:35 -0700

    working

--- ttsServer/Sources/myapi.swift
+++ ttsServer/Sources/myapi.swift
@@ -5,19 +5,71 @@ import SwiftyJSON
 class MyApi {
     
     var parameters = [String:String]()
+    var str = ""
 
     var router: Router?
     
     init() {
-        //let viewCont = ViewController()
         self.router = Router()
         router?.all(middleware: BodyParser())
+    }
+    
+    func callWatson (completionHandler: @escaping (String)->(), parameters: ([String:String]) ) -> String {
+        
+        print(parameters)
+        guard let url = URL(string: "https://watson-api-explorer.mybluemix.net/language-translator/api/v2/translate") else {return "error"}
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return "error"}
+        request.httpBody = httpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if response != nil {
+                print("There is a response, but it is too long and annoying to view in the console. ")
+            }
+            
+            if let data = data {
+                
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String : AnyObject]
+                    if let translation = json["translations"]  {
+                        for index in 0...translation.count-1 {
+                            let aObject = translation[index] as! [String : AnyObject]
+                            let something = String(describing: aObject)
+                            //cut out useless characters from JSON response
+                            let tempStr = String(something.characters.dropLast(1))
+                            let finalStr = String(tempStr.characters.dropFirst(16))
+                            completionHandler(finalStr)
+                        }
+                    }
+                    
+                }
+                catch {
+                    print(error)
+                }
+                
+                          }
+            }.resume()
+        return "error"
     }
     
     func myApiRoutes() -> Router {
       
         //POST REQUEST
-        router?.post("/") { request, response, next in
+        router?.post("/translate") { request, response, next in
+            
+//            guard let body = request.body,
+//                let json = body.asJSON,
+//            let name = json["text"].string
+//            else {
+//                try response.status(.badRequest).end()
+//                return
+//            }
+//            
+//            response.send(name)
+//            next()
             
             guard let postData = request.body else {next(); return}
             
@@ -29,74 +81,25 @@ class MyApi {
                 let textToTranslate = jsonData["text"].string ?? ""
                 if sourceLang == "English" {
                     self.parameters["source"] = "en"
-                    //response.send("en")
                 }
                 if targetLang == "Spanish" {
                     self.parameters["target"] = "es"
-                    //response.send("es")
                 }
-                if textToTranslate == "Hello" {
-                    self.parameters["text"] = "Hello"
-                    //response.send("Hello")
+                if textToTranslate == "something cool" {
+                    self.parameters["text"] = textToTranslate
                 }
-                //print(self.parameters)
             default:
                 try response.send("Default").end()
             }
             
-            self.callWatson(parameters: self.parameters)
-            print("Returned")
+            let pfd = MyApi()
+            pfd.callWatson(completionHandler: { (jsonString) in
+                response.send(jsonString)
+            }, parameters: self.parameters)
+            
         }
-        print("Returned")
         return router!
     }
     
-    func callWatson ( parameters: ([String:String]) ) {
-        print(parameters)
-        guard let url = URL(string: "https://watson-api-explorer.mybluemix.net/language-translator/api/v2/translate") else {return}
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
-        request.httpBody = httpBody
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        let session = URLSession.shared
-        session.dataTask(with: request) { (data, response, error) in
-            //print(type(response))
-            if let response = response {
-                print(response)
-            }
-            
-            if let data = data {
-                self.formatData(data: data)
-            }
-        }.resume()
     
-    
-    return
-    }
-    
-    func formatData(data: Data) {
-        print("$$$$$ Data: ")
-        print(data)
-        
-        do {
-            let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String : AnyObject]
-            if let translation = json["translations"]  {
-                for index in 0...translation.count-1 {
-                    let aObject = translation[index] as! [String : AnyObject]
-                    let something = String(describing: aObject)
-                    //cut out useless characters from JSON response
-                    let tempStr = String(something.characters.dropLast(1))
-                    let finalStr = String(tempStr.characters.dropFirst(16))
-                    print("final STRING: \(finalStr)")
-                }
-            }
-            
-        }
-        catch {
-            print(error)
-        }
-
-    }
 }
