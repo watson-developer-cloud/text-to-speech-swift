194d705c3956345fd12c8fbaee00ce7e9f105510
Horea Porutiu <horeaporutiu@gmail.com>
2017-06-11 21:09:43 -0700

    changed to one file

--- ttsServer/Sources/main.swift
+++ ttsServer/Sources/main.swift
@@ -1,11 +1,79 @@
 import Kitura
 import HeliumLogger
+import Foundation
+import SwiftyJSON
 
-// Initialize HeliumLogger
 HeliumLogger.use()
 
-let api = MyApi()
+let router = Router()
+router.post(middleware: BodyParser())
+var parameters = [String:String]()
 
-Kitura.addHTTPServer(onPort: 8080, with: api.myApiRoutes())
+func callWatson (completionHandler: @escaping (String)->(), parameters: ([String:String]) ) -> String {
+    
+    guard let url = URL(string: "https://watson-api-explorer.mybluemix.net/language-translator/api/v2/translate") else {return "error"}
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return "error"}
+    request.httpBody = httpBody
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    let session = URLSession.shared
+    session.dataTask(with: request) { (data, response, error) in
+        if response != nil {}
+        
+        if let data = data {
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String : AnyObject]
+                if let translation = json["translations"]  {
+                    for index in 0...translation.count-1 {
+                        let aObject = translation[index] as! [String : AnyObject]
+                        let something = String(describing: aObject)
+                        //cut out useless characters from JSON response
+                        let tempStr = String(something.characters.dropLast(1))
+                        let finalStr = String(tempStr.characters.dropFirst(16))
+                        completionHandler(finalStr)
+                    }
+                }
+                
+            }
+            catch {
+                print(error)
+            }
+            
+        }
+        }.resume()
+    return "error"
+}
 
+func myApiRoutes() -> Router {
+    
+    //POST REQUEST
+    router.post("/translates") { request, response, next in
+        
+        guard let body = request.body,
+            let json = body.asJSON,
+            let textToTranslate = json["text"].string,
+            let targetLang = json["target"].string,
+            let sourceLang = json["source"].string
+            else {
+                try response.status(.badRequest).end()
+                return
+        }
+        
+        parameters["source"] = sourceLang
+        parameters["target"] = targetLang
+        parameters["text"] = textToTranslate
+        
+        callWatson(completionHandler: { (jsonString) in
+            response.send(jsonString)
+        }, parameters: parameters)
+        
+    }
+    return router
+}
+myApiRoutes()
+
+Kitura.addHTTPServer(onPort: 8080, with: router)
 Kitura.run()
